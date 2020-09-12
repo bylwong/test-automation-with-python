d4db775a077d20b419aa4d483ac2f76916f92123
Benny Wong <bylwong@gmail.com>
2020-09-12 15:36:45 -0700

    interacting with input forms, and find by class name

--- test_module.py
+++ test_module.py
@@ -10,5 +10,18 @@ driver.get("https://www.demoblaze.com/")
 login_button = driver.find_element_by_id("login2")
 login_button.click()
 
+sleep(1)
+login_modal = driver.find_element_by_id("logInModal")
+username_input = login_modal.find_element_by_id("loginusername")
+username_input.clear()
+username_input.send_keys("a")
+
+password_input = login_modal.find_element_by_id("loginpassword")
+password_input.clear()
+password_input.send_keys("a")
+
+login_submit = login_modal.find_element_by_class_name("btn-primary")
+login_submit.click()
+
 sleep(5)
 driver.quit()
