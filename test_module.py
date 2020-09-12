a33eba3fca436087fac8cb26815f6a114dea3ecb
Benny Wong <bylwong@gmail.com>
2020-09-12 10:34:39 -0700

    select element by id fixed window size

--- test_module.py
+++ test_module.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 path = ChromeDriverManager().install()
 driver = webdriver.Chrome(path)
+driver.set_window_size(1024, 800)
 driver.get("https://www.demoblaze.com/")
 
 login_button = driver.find_element_by_id("login2")
